26ecb125eb23591af4f81a46e84e781fb7046345
Faraz Nadeem <muhammad.ali.faraz@gmail.com>
2023-08-21 17:07:53 -0400

    Redone

--- easyProblems/questionTwo/solutionTwo.cpp
+++ easyProblems/questionTwo/solutionTwo.cpp
@@ -1,73 +1,26 @@
-#include <string> 
-#include <cmath>
-#include <iostream>
-
 class Solution {
 public:
-    bool isPalindrome(int x) 
-    {
-        int numDigits = std::to_string(x).size()/2;
-        int numberOfDigitsTotal = std::to_string(x).size();
-        if ( numberOfDigitsTotal < 3)
-        {
-            if (numberOfDigitsTotal == 1)
-            {
-                return true;
-            }
-            else if (numberOfDigitsTotal == 2)
-            {
-                std::cout<<"We are here.\n";
-                int firstDigit = x /10;
-                int secondDigit = x % 10;
-                if (firstDigit == secondDigit)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-
-            }
-
-        }   
-        
-        int powerOfTen = std::pow(10 , numDigits);
-        int firstHalf = x % powerOfTen;
-        if (firstHalf == reversedDigit(x))
-        {
-            return true;
+    bool isPalindrome(int x) {
+        // Check if the input number is negative, in which case it's not a palindrome
+        if (x < 0) {
+            return false;
         }
-        else
-        {
+        
+        int digit = x;      // Store the original number for later comparison
+        long reversed = 0;  // Initialize the reversed number
+        
+        // Reversing the digits of the number
+        while (digit != 0) {
+            int number = digit % 10;          // Extract the last digit
+            reversed = reversed * 10 + number; // Add the digit to the reversed number
+            digit = digit / 10;               // Remove the last digit from the original number
+        }
+        
+        // Compare the reversed number with the original number
+        if (reversed == x) {
+            return true;
+        } else {
             return false;
         }
     }
-
-    int reversedDigit(int number)
-    {
-        int numDigits = std::to_string(number).size()/2;
-        int reversed = 0;
-
-        int powerOfTen = std::pow(10, numDigits);
-        int digit = number % powerOfTen;
-        //std::cout << number << " % " << powerOfTen << " = " << digit << std::endl;
-
-        while (digit != 0)
-        {
-            int number = digit % 10;
-            reversed = reversed * 10 + number;
-            digit = digit /10;
-        }
-
-        //std::cout<< "Reversed = " << reversed <<std::endl;
-        return reversed;
-    }
 };
-
-int main()
-{
-    Solution solution;
-    std::cout<<solution.isPalindrome(10);
-    return 0;
-}
--- easyProblems/questionTwo/solutionTwo.py
+++ easyProblems/questionTwo/solutionTwo.py
@@ -1,46 +1,20 @@
 class Solution(object):
 
-    def reverseDigits(self, number:int) -> int:
-        numDigits = len(str(number))//2
-        reversedNum = 0
-
-        powerOfTen = 10 ** numDigits
-        digit = number % powerOfTen
-
-        # Reverse this 
-        while digit != 0:
+    def reverseDigits(self, x:int) -> int:
+        if (x < 0):
+            return False
+        digit = x
+        reversed = 0
+        while(digit != 0):
             number = digit % 10
-            reversedNum = reversedNum * 10 + number
-            digit = digit //10 
-
-        return reversedNum
-
-
-    def isPalindrome(self, x:int) -> bool:
-        numDigits = len(str(x))//2
-        numberOfDigits = len(str(x))
-        # Take care of edge cases 
-        if (numberOfDigits == 1):
-            return True
-        elif (numberOfDigits == 2):
-            firstDigit = x // 10
-            secondDigit = x % 10
-            if firstDigit == secondDigit:
-                return True
-            else:
-                return False
-        # Lets get to the problem
-        powerOfTen = 10 ** numDigits
-        firstHalf = x % powerOfTen
-        secondHalf = self.reverseDigits(x)
-
-        if firstHalf == secondHalf:
+            reversed = reversed * 10 + number
+            digit = digit /10
+        if (reversed == x):
             return True
         else:
             return False
         
-
-
+         
 
 
 
