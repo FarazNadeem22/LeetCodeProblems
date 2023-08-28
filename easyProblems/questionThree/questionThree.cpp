2906e245bb86797219e3f1181180b0ddb1ba3151
Faraz Nadeem <muhammad.ali.faraz@gmail.com>
2023-08-28 14:49:39 -0400

    commit

--- easyProblems/questionThree/questionThree.cpp
+++ easyProblems/questionThree/questionThree.cpp
@@ -2,26 +2,28 @@
 #include <vector>
 #include <algorithm>
 
+
+
 class Solution {
 public:
     int maxProfit(std::vector<int>& prices) {
         int maxProfit = 0;
         auto buyPriceIterator = std::max_element(prices.begin(), prices.end());
         int buyPrice = *buyPriceIterator;
-        int buyIndex = 0;
+        int buyIndex;
         int sellPrice = 0;
         bool firstRun = true;
 
         // For buy Price 
         for (int i = 0; i < prices.size(); ++i) {
             if (firstRun) {
-                if (i + 1 < prices.size() && prices[i] < prices[i + 1]) {
+                if (prices[i] < prices[i + 1]) {
                     buyPrice = prices[i];
                     buyIndex = i;
                     firstRun = false;
                 }
             }
-            if (i + 1 < prices.size() && (prices[i] < prices[i + 1]) && (prices[i] < buyPrice)) {
+            if ((prices[i] < prices[i + 1]) && (prices[i] < buyPrice)) {
                 buyPrice = prices[i];
                 sellPrice = prices[i];
                 buyIndex = i;
@@ -29,25 +31,26 @@ public:
         }
 
         // For sell price
-        for (int i = buyIndex; i < prices.size(); ++i) {
-            if (prices[i] > sellPrice) {
+        for (int i = buyIndex; i < prices.size() ; ++i) {
+            if (prices[i] < sellPrice) {
                 sellPrice = prices[i];
             }
         }
-    
+    }
         maxProfit = sellPrice - buyPrice;
-        std::cout << sellPrice << " - " << buyPrice << " = " << maxProfit << ", buy Index: " << buyIndex;
+        std::cout<<sellPrice<<" - "<< buyPrice << " = " << maxProfit <<", buy Index: "<< buyIndex;
         return maxProfit;
     }
 };
 
-int main() {
-    std::vector<int> priceVector = {7, 1, 5, 3, 6, 4};
+int main()
+{
+    std::vector<int> priceVector = {7,1,5,3,6,4};
     int result;
 
     Solution solution;
     result = solution.maxProfit(priceVector);
-    std::cout << result;
+    std::cout<< result;
     
     return 0;
 }
